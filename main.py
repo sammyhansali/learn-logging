6d0bcdfe65fdc76f61ba58e730fb616b53dcb605
Sammy Hansali <sammyhansali@gmail.com>
2025-10-19 14:43:36 -0400

    better

--- main.py
+++ main.py
@@ -1,30 +1,45 @@
 import logging.config
 
 # logging.debug() # BAD because it uses the root logger
-logging.getLogger("my_app") # GOOD because it uses one of your loggers
+logger = logging.getLogger("my_app") # GOOD because it uses one of your loggers
 
 logging_config = {
     "version": 1,
     "disable_existing_loggers": False,
-    "filters": {},
-    "formatters": {},
-    "handlers": {},
-    "loggers": {},
+    # "filters": {},
+    "formatters": {
+        "simple": {
+            "format": "%(levelname)s | %(name)s | %(message)s."
+        }
+    },
+    "handlers": {
+        "stdout": {
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+            "stream": "ext://sys.stdout"
+        }
+    },
+    "loggers": {
+        "root": {
+            "level": "DEBUG",
+            "handlers": ["stdout"]
+        }
+    },
 }
 
 def main():
     # logging.basicConfig(level=logging.DEBUG)
     logging.config.dictConfig(config=logging_config) # Use dictConfig()
 
-    logging.debug("debug message")
-    logging.info("info message")
-    logging.warning("warning message")
-    logging.error("error message")
-    logging.critical("critical message")
+    logger.debug("debug message")
+    logger.info("info message")
+    logger.warning("warning message")
+    logger.error("error message")
+    logger.critical("critical message")
     try:
         1/0
     except ZeroDivisionError:
-        logging.exception("exception message")
+        logger.exception("exception message")
 
 
 if __name__ == "__main__":
