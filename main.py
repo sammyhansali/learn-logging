b25dabf83f36199ed9532d6c6a64824df6ff5bfd
Sammy Hansali <sammyhansali@gmail.com>
2025-10-18 20:31:49 -0400

    typical logging

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import logging
+
+logging.getLogger("my_app")
+
 def main():
-    print("Hello from learn-logging!")
+    logging.basicConfig(level=logging.DEBUG)
+    logging.debug("debug message")
+    logging.info("info message")
+    logging.warning("warning message")
+    logging.error("error message")
+    logging.critical("critical message")
+    try:
+        1/0
+    except ZeroDivisionError:
+        logging.exception("exception message")
 
 
 if __name__ == "__main__":
