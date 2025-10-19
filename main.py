fd7de464f50ef5cef163bc0fa14bbc241cc24f75
Sammy Hansali <sammyhansali@gmail.com>
2025-10-19 15:21:48 -0400

    good stopping point for most applications

--- main.py
+++ main.py
@@ -1,35 +1,20 @@
+import json
+import pathlib
 import logging.config
 
 # logging.debug() # BAD because it uses the root logger
 logger = logging.getLogger("my_app") # GOOD because it uses one of your loggers
 
-logging_config = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    # "filters": {},
-    "formatters": {
-        "simple": {
-            "format": "%(levelname)s | %(name)s | %(message)s."
-        }
-    },
-    "handlers": {
-        "stdout": {
-            "class": "logging.StreamHandler",
-            "formatter": "simple",
-            "stream": "ext://sys.stdout"
-        }
-    },
-    "loggers": {
-        "root": {
-            "level": "DEBUG",
-            "handlers": ["stdout"]
-        }
-    },
-}
+def logging_setup():
+    config_file = pathlib.Path("logging_configs/medium.json")
+    with open(config_file, "r") as f:
+        config = json.load(f)
+    logging.config.dictConfig(config=config)
+
 
 def main():
     # logging.basicConfig(level=logging.DEBUG)
-    logging.config.dictConfig(config=logging_config) # Use dictConfig()
+    logging_setup()
 
     logger.debug("debug message")
     logger.info("info message")
